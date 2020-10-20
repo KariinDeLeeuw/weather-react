d3b589571095295253551b2791514544e9318ee4
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2020-10-21 01:32:47 +0200

    fixed bug

--- src/App.js
+++ src/App.js
@@ -12,20 +12,20 @@ export default function App() {
         <footer>
           <p>
           This project was coded by{" "}
-          <a href="https://www.linkedin.com/in/kariindeleeuw/" target="_blank">
+          <a href="https://www.linkedin.com/in/kariindeleeuw/" target="_blank" rel="noopener noreferrer">
             Karin de Leeuw
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/KariinDeLeeuw/weather-react"
-            target="_blank" 
+            target="_blank" rel="noopener noreferrer"
           >
             open-sourced on GitHub
           </a>{" "}
           and{" "}
           <a
             href="https://inspiring-leakey-6ac9dc.netlify.app/"
-            target="_blank" 
+            target="_blank" rel="noopener noreferrer"
           >
             hosted on Netlify
           </a>
